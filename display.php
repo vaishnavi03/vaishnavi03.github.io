39f7bf9c09ca3399087c36284f2f0b9df187f4fa
vaishnavi mundhe <37343887+vaishnavi03@users.noreply.github.com>
2019-03-05 22:16:30 +0530

    Add files via upload

---
+++ display.php
@@ -0,0 +1,70 @@
+<head>
+<style>
+.quickaddform{
+display:none;
+}
+
+body{
+
+  background-size: cover;
+  height: 100%;
+  
+   background-image: url("https://images.unsplash.com/photo-1470790376778-a9fbc86d70e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60");
+}h2{
+     color: #826922;
+    text-shadow: -1px -1px 1px #000, 1px 1px 1px #ccc;
+}
+button{
+	background-color: #555555;
+	font-size: 20px;
+	border-radius: 12px;
+	padding: 10px;
+}
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: black;
+  color: white;
+  text-align: right;
+}
+</style>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Phone Directory</a>
+    </div>
+</nav>
+
+<?php
+$cnt=mysqli_connect("localhost","root","");
+mysqli_select_db($cnt,"phone_directory");
+$result=mysqli_query($cnt,"SELECT * FROM subscribers");
+if (mysqli_num_rows($result) > 0) {
+    echo "<h2>Subscriber Details:</h2>";
+    while($row = mysqli_fetch_assoc($result)) {
+        echo " <h3>Name:" . $row["Name"]. "  <br>  " ."Contact:". $row["Phone"]. "</h3><br>";
+    }
+} else {
+    echo "0 results";
+}
+mysqli_close($cnt);
+?>
+<br>
+<form action="home.html">
+<button type="submit">BACK</button>
+</form>
+<div class="footer">
+  <p>Vaishnavi Mundhe(m.vaishnavis03@gmail.com)</p>
+</div>
+</body>
